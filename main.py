2043c890cfcde5d133b1ce1bf16313e124a133f5
Riley-Richards <18204@burnside.school.nz>
2021-05-24 09:51:59 +1200

    added database to website

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def home():
 def postion():
   conn=sqlite3.connect('football.db')
   cursor=conn.cursor()
-  cursor.execute('SELECT * FROM Position WHERE id=?;',(id,))
-  position=cursor.fetchone()
+  cursor.execute('SELECT names FROM Position')
+  position=cursor.fetchall()
   conn.close()
   return render_template('position.html', position=position, title="Position")
   
