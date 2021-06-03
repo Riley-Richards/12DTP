ec490db2be5809eff9c5d011d20c3b9b13a3b33a
Riley-Richards <18204@burnside.school.nz>
2021-06-04 10:26:35 +1200

    added individual pages for positions, teams and players

--- main.py
+++ main.py
@@ -11,28 +11,55 @@ def home():
 def postion():
   conn=sqlite3.connect('football.db')
   cursor=conn.cursor()
-  cursor.execute('SELECT names FROM Position')
-  position=cursor.fetchall()
+  cursor.execute('SELECT id, names FROM Position')
+  positions=cursor.fetchall()
   conn.close()
-  return render_template('position.html', position=position, title="Positions")
+  return render_template('position.html', positions=positions, title="Positions")
+
+@app.route("/position/<int:id>")
+def positionid(id):
+  conn=sqlite3.connect('football.db')
+  cursor=conn.cursor()
+  cursor.execute('SELECT * FROM Position where id=?;', (id,))
+  positionid=cursor.fetchall()
+  conn.close()
+  return render_template('positionid.html', positionid=positionid, title="Position")
 
 @app.route("/player")
 def player():
   conn=sqlite3.connect('football.db')
   cursor=conn.cursor()
-  cursor.execute('SELECT name FROM Player')
-  player=cursor.fetchall()
+  cursor.execute('SELECT id, name FROM Player')
+  players=cursor.fetchall()
   conn.close()
-  return render_template('player.html', player=player, title="Player")
+  return render_template('player.html', players=players, title="Players")
+
+@app.route("/player/<int:id>")
+def playerid(id):
+  conn=sqlite3.connect('football.db')
+  cursor=conn.cursor()
+  cursor.execute('SELECT name, info FROM Player where id=?;', (id,))
+  playerid=cursor.fetchall()
+  conn.close()
+  return render_template('playerid.html', playerid=playerid, title="Player")
 
 @app.route("/team")
 def team():
   conn=sqlite3.connect('football.db')
   cursor=conn.cursor()
-  cursor.execute('SELECT name FROM Team')
-  team=cursor.fetchall()
+  cursor.execute('SELECT id, name FROM Team')
+  teams=cursor.fetchall()
   conn.close()
-  return render_template('team.html', team=team, title="Team")
+  return render_template('team.html', teams=teams, title="Team")
+
+@app.route("/team/<int:id>")
+def teamid(id):
+  conn=sqlite3.connect('football.db')
+  cursor=conn.cursor()
+  cursor.execute('SELECT name, info FROM Team where id=?;', (id,))
+  teamid=cursor.fetchall()
+  conn.close()
+  return render_template('teamid.html', teamid=teamid, title="Team")
 
 @app.route("/trophy")
 def trophy():
@@ -43,14 +70,5 @@ def trophy():
   conn.close()
   return render_template('trophy.html', trophy=trophy, title="Trophy")
 
-@app.route("/position/<int:id>")
-def positionid(id):
-  conn=sqlite3.connect('football.db')
-  cursor=conn.cursor()
-  cursor.execute('SELECT names FROM Position where id=?;', (id,))
-  positionid=cursor.fetchall()
-  conn.close()
-  return render_template('positionid.html', positionid=positionid, title="Position")
-
 if __name__ == '__main__':
-  app.run(port=8080, debug=True)
+  app.run(port=8080, debug=True)  
