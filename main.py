a11ec7afcb0c5ec52c8a111b4e69bd66ad4d8d30
Riley-Richards <18204@burnside.school.nz>
2021-07-02 10:27:13 +1200

    cool

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Simplify SQL connections for each route
 def do_query(query, data = None, fetchall = False):
     conn = sqlite3.connect('football.db')
     cursor = conn.cursor()
@@ -32,17 +33,19 @@ def postionid(id):
   return render_template('positionid.html', positionid=positionid, title="position")
 
 #all players page route
-@app.route("/player")
-def player():
+@app.route("/players")
+def players():
   players = do_query('SELECT id, name, image FROM Player', fetchall=True)
-  return render_template('player.html', players=players, title="Players")
+  footballteams = do_query("SELECT name FROM Team WHERE id IN (SELECT cid FROM PlayerTeams WHERE fid = id)", fetchall=True)  
+  return render_template('player.html', players=players, footballteams=footballteams, title="Players")
 
 #individual player page route
 @app.route("/player/<int:id>")
 def playerid(id):
   playerid = do_query('SELECT name, info, image FROM Player where id=?;', (id,), fetchall=False)
-  playertrophies = do_query("SELECT name FROM Trophies WHERE id IN (SELECT tid FROM PlayerTrophies WHERE fid = ?)", (id,), fetchall=True)
-  return render_template('playerid.html', playerid=playerid, playertrophies=playertrophies, title="player")
+  playertrophies = do_query("SELECT id, name FROM Trophies WHERE id IN (SELECT tid FROM PlayerTrophies WHERE fid = ?)", (id,), fetchall=True)
+  playerteams = do_query("SELECT id, name FROM Team WHERE id IN (SELECT cid FROM PlayerTeams WHERE fid = ?)", (id,), fetchall=True)
+  return render_template('playerid.html', playerid=playerid, playertrophies=playertrophies, playerteams=playerteams, title="player")
 
 #all teams page route
 @app.route("/team")
@@ -55,7 +58,8 @@ def team():
 def teamid(id):
   teamid = do_query('SELECT * FROM Team where id=?;', (id,), fetchall=False)
   teamtrophies = do_query("SELECT name FROM Trophies WHERE id IN (SELECT tid FROM TeamTrophies WHERE cid = ?)", (id,), fetchall=True)
-  return render_template('teamid.html', teamid=teamid, teamtrophies=teamtrophies, title="team")
+  teamplayers = do_query("SELECT id, name FROM Player WHERE id IN (SELECT fid FROM PlayerTeams WHERE cid = ?)", (id,), fetchall=True)
+  return render_template('teamid.html', teamid=teamid, teamtrophies=teamtrophies, teamplayers=teamplayers, title="team")
 
 #all trophies page route
 @app.route("/trophy")
@@ -73,6 +77,8 @@ def trophyid(id):
 @app.route("/squadbuilder")
 def squadbuilder():
   return render_template('squad.html', title="Squadbuilder")
+
+
 
 if __name__ == '__main__':
  app.run(port=8080, debug=True)  
