0a5d974d013743fa2383779688cf2330883839db
Riley-Richards <18204@burnside.school.nz>
2021-10-21 08:56:58 +1300

    added feature across whole project

--- main.py
+++ main.py
@@ -5,82 +5,142 @@ app = Flask(__name__)
 
 
 # Simplify SQL connections for each route
-def do_query(query, data = None, fetchall = False):
+def do_query(query, data=None, fetchall=False):
     conn = sqlite3.connect('football.db')
     cursor = conn.cursor()
     if data is None:
         cursor.execute(query)
     else:
-        cursor.execute(query,data)
+        cursor.execute(query, data)
     results = cursor.fetchall() if fetchall else cursor.fetchone()
     conn.close()
     return results
 
-#home page route
+
+# home page route
 @app.route('/')
 def home():
-  return render_template('home.html', title="Home")
+    return render_template('home.html', title="Home")
 
-#all positions page route
+
+# all positions page route
 @app.route("/position")
 def postion():
-  return render_template('position.html', title="position")
+    return render_template('position.html', title="position")
 
-#individual position page route
+
+# individual position page route
 @app.route("/position/<int:id>")
 def postionid(id):
-  positionid = do_query('SELECT * FROM Position where id=?;', (id,), fetchall=False)
-  playerp = do_query('SELECT * FROM Player where pid=?;', (id,), fetchall=True)
-  return render_template('positionid.html', positionid=positionid, playerp=playerp, title="position")
+    positionid = do_query('SELECT * FROM Position where id=?;', (id, ),
+                          fetchall=False)
+    playerp = do_query('SELECT * FROM Player where pid=?;', (id, ),
+                       fetchall=True)
+    return render_template('positionid.html',
+                           positionid=positionid,
+                           playerp=playerp,
+                           title="position")
 
-#all players page route
+
+# all players page route
 @app.route("/players")
 def players():
-  players = do_query('SELECT id, name, image FROM Player', fetchall=True)
-  footballteams = do_query("SELECT name FROM Team WHERE id IN (SELECT cid FROM PlayerTeams WHERE fid = id)", fetchall=True)  
-  return render_template('player.html', players=players, footballteams=footballteams, title="Players")
+    players = do_query('SELECT id, name, image FROM Player', fetchall=True)
+    return render_template('player.html',
+                           players=players,
+                           title="Players")
 
-#individual player page route
+
+# individual player page route
 @app.route("/player/<int:id>")
 def playerid(id):
-  playerid = do_query('SELECT name, info, image, nation FROM Player where id=?;', (id,), fetchall=False)
-  playertrophies = do_query("SELECT id, name, image FROM Trophies WHERE id IN (SELECT tid FROM PlayerTrophies WHERE fid = ?)", (id,), fetchall=True)
-  playerteams = do_query("SELECT * FROM Team WHERE id IN (SELECT cid FROM PlayerTeams WHERE fid = ?)", (id,), fetchall=True)
-  return render_template('playerid.html', playerid=playerid, playertrophies=playertrophies, playerteams=playerteams, title="player")
+    playerid = do_query(
+        'SELECT name, info, image, nation FROM Player where id=?;', (id, ),
+        fetchall=False)
+    playertrophies = do_query(
+        """SELECT id, name, image FROM Trophies
+        WHERE id IN (SELECT tid FROM PlayerTrophies
+        WHERE fid = ?)""",
+        (id, ),
+        fetchall=True)
+    playerteams = do_query(
+        """SELECT id, name, image FROM Team
+        WHERE id IN (SELECT cid FROM PlayerTeams
+        WHERE fid = ?)""",
+        (id, ),
+        fetchall=True)
+    return render_template('playerid.html',
+                           playerid=playerid,
+                           playertrophies=playertrophies,
+                           playerteams=playerteams,
+                           title="player")
 
-#all teams page route
+
+# all teams page route
 @app.route("/team")
 def team():
-  teams = do_query('SELECT * FROM Team', fetchall=True)
-  return render_template('team.html', teams=teams, title="Teams")
+    teams = do_query('SELECT * FROM Team', fetchall=True)
+    return render_template('team.html', teams=teams, title="Teams")
 
-#individual teams page route
+
+# individual teams page route
 @app.route("/team/<int:id>")
 def teamid(id):
-  teamid = do_query('SELECT * FROM Team where id=?;', (id,), fetchall=False)
-  teamtrophies = do_query("SELECT id, name, image FROM Trophies WHERE id IN (SELECT tid FROM TeamTrophies WHERE cid = ?)", (id,), fetchall=True)
-  teamplayers = do_query("SELECT id, name, image FROM Player WHERE id IN (SELECT fid FROM PlayerTeams WHERE cid = ?)", (id,), fetchall=True)
-  return render_template('teamid.html', teamid=teamid, teamtrophies=teamtrophies, teamplayers=teamplayers, title="team")
+    teamid = do_query('SELECT * FROM Team where id=?;', (id, ), fetchall=False)
+    teamtrophies = do_query(
+        """SELECT id, name, image FROM Trophies WHERE
+        id IN (SELECT tid FROM TeamTrophies WHERE cid = ?)""",
+        (id, ),
+        fetchall=True)
+    teamplayers = do_query(
+        """SELECT id, name, image FROM Player WHERE id IN
+        (SELECT fid FROM PlayerTeams WHERE cid = ?)""",
+        (id, ),
+        fetchall=True)
+    return render_template('teamid.html',
+                           teamid=teamid,
+                           teamtrophies=teamtrophies,
+                           teamplayers=teamplayers,
+                           title="team")
 
-#all trophies page route
+
+# all trophies page route
 @app.route("/trophy")
 def trophy():
-  trophy = do_query('SELECT id, name, image FROM Trophies', fetchall=True)
-  return render_template('trophy.html', trophy=trophy, title="trophy")
+    trophy = do_query('SELECT id, name, image FROM Trophies', fetchall=True)
+    return render_template('trophy.html', trophy=trophy, title="trophy")
 
-#individual trophy page route
+
+# individual trophy page route
 @app.route("/trophy/<int:id>")
 def trophyid(id):
-  trophyid = do_query('SELECT * FROM Trophies where id=?;', (id,), fetchall=False)
-  trophyplayers = do_query("SELECT id, name, image FROM Player WHERE id IN (SELECT fid FROM PlayerTrophies WHERE tid = ?)", (id,), fetchall=True)
-  trophyteams = do_query("SELECT id, name, image FROM Team WHERE id IN (SELECT fid FROM TeamTrophies WHERE tid = ?)", (id,), fetchall=True)
-  return render_template('trophyid.html', trophyid=trophyid, trophyplayers=trophyplayers, trophyteams=trophyteams, title="trophy")
+    trophyid = do_query('SELECT * FROM Trophies where id=?;', (id, ),
+                        fetchall=False)
+    trophyplayers = do_query(
+        """SELECT id, name, image FROM Player WHERE id IN
+        (SELECT fid FROM PlayerTrophies WHERE tid = ?)""",
+        (id, ),
+        fetchall=True)
+    trophyteams = do_query(
+        """SELECT id, name, image FROM Team
+        WHERE id IN (SELECT cid
+        FROM TeamTrophies WHERE tid = ?)""",
+        (id, ),
+        fetchall=True)
+    return render_template('trophyid.html',
+                           trophyid=trophyid,
+                           trophyplayers=trophyplayers,
+                           trophyteams=trophyteams,
+                           title="trophy")
 
+
+# squadbuilder route
 @app.route("/squadbuilder")
 def squadbuilder():
-     return render_template('squad.html')
+    return render_template('squad.html')
 
-#squad builder page route
+
+# squad builder page route
 @app.route("/squad", methods=["POST"])
 def squad():
     connection = sqlite3.connect('football.db')
@@ -88,7 +148,8 @@ def squad():
     name = request.form["name"]
     gk = request.form["gk"]
     rb = request.form["rb"]
-    cb = request.form["cb"]
+    center_back = request.form["center_back"]
+    lcb = request.form["lcb"]
     lb = request.form["lb"]
     cdm = request.form["cdm"]
     cm = request.form["cm"]
@@ -96,29 +157,51 @@ def squad():
     lw = request.form["lw"]
     rw = request.form["rw"]
     st = request.form["st"]
-    sql = "INSERT INTO squadbuilder(name, gk, lb, cb, rb, cdm, cm, cam, lw, rw, st) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    cursor.execute(sql,(name, gk, lb, cb, rb, cdm, cm, cam, lw, rw, st))
+    sql = """INSERT INTO squadbuilder
+    (name, gk, lb, rcb, lcb, rb, cdm, cm, cam, lw, rw, st)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+    cursor.execute(
+        sql, (name, gk, lb, center_back, lcb, rb, cdm, cm, cam, lw, rw, st))
     connection.commit()
     connection.close()
+    submit = True
     return render_template('squad.html')
 
-@app.route("/allsquads")
-  def allsquads():
-    allsquads = do_query("SELECT id, name FROM squad")
-    return render_template('allsquads.html')
 
-#404 ERROR page
+# page to display all user created teams
+@app.route("/allsquads")
+def allsquads():
+    allsquads = do_query("SELECT id, name FROM squadbuilder",
+                         data=None,
+                         fetchall=True)
+    return render_template('allsquads.html', allsquads=allsquads)
+
+
+# page to display individual user created team
+@app.route("/squad/<int:id>")
+def user_squad(id):
+    user_squad = do_query('SELECT * FROM squadbuilder where id=?;', (id, ),
+                          fetchall=False)
+    return render_template('usersquad.html', user_squad=user_squad)
+
+
+# 404 ERROR page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
 
 
+# contact route
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title = 'Contact')
+    return render_template('contact.html', title='Contact')
 
+
+# page for user to input contact details and message
 submit = None
-@app.route ("/message", methods=["POST"])
+
+
+@app.route("/message", methods=["POST"])
 def message():
     connection = sqlite3.connect('football.db')
     cursor = connection.cursor()
@@ -126,14 +209,35 @@ def message():
     user_last_name = request.form["user_last_name"]
     user_email = request.form["user_email"]
     user_message = request.form["user_message"]
-    sql = "INSERT INTO contact(user_first_name, user_last_name, user_email, user_message) VALUES (?, ?, ?, ?)"
-    cursor.execute(sql,(user_first_name, user_last_name, user_email, user_message))
+    sql = """INSERT INTO contact
+    (user_first_name, user_last_name, user_email, user_message)
+    VALUES (?, ?, ?, ?)"""
+    cursor.execute(sql,
+                   (user_first_name, user_last_name, user_email, user_message))
     connection.commit()
     connection.close()
     submit = True
-    return render_template('contact.html', submit = submit)
+    return render_template('contact.html', submit=submit)
 
 
+# search bar for searching players
+@app.route('/search', methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        print(request.form.get("filter"))
+        search = do_query(
+            f'''SELECT * FROM Player WHERE Player.name
+            LIKE "%" || ? || "%" ORDER BY Player.name;''',
+            (request.form.get("filter"), ),
+            fetchall=True)
+        if len(search) == 0:
+            return redirect('/error')
+        elif request.form.get("filter") == '':
+            return redirect('/error')
+        else:
+            return redirect(f'/player/{(search[0])[0]}')
 
+
+# runs port on local site
 if __name__ == '__main__':
- app.run(port=8080, debug=True)  
+    app.run(port=8080, debug=True)
